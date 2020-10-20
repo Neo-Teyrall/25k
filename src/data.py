ef22df4b1ed10921bee7d213c7b0e5f17859d7f4
Neo-Teyrall <mailliw.marg@gmail.com>
2020-10-20 22:11:18 +0200

    ajoue du model janus et complétion de la sauvegarde du model et de ses sortie

--- src/data.py
+++ src/data.py
@@ -4,6 +4,7 @@ import copy
 import math
 import statistics
 import datetime
+import json
 
 def one_hot_AA_seq(seq: str):
     out = []
@@ -98,7 +99,6 @@ def normer(vec):
         return norm(vec)
 
 def norm(vec):
-    #print(vec[:5],"\n")
     m = statistics.mean(vec)
     std = statistics.stdev(vec)
     for i in range(len(vec)):
@@ -110,24 +110,31 @@ def norm(vec):
 
 def mat_input(jsons):
     data_out  = []
-    for i in jsons :
-        ohaa  = one_hot_AA_seq(i["sequence"])
-        ohsty = one_hot_struct_seq(i["predicted_loop_type"])
-        ohst = one_hot_structure(i["structure"])
+    list_mat = []
+    for i,json in enumerate(jsons) :
+        print("loading input : {:.2f} %".format((i+1)/len(jsons)*100),end = "\r")
+        mat = numpy.load("../data/bpps/"+ json["id"]+".npy")
+        ohaa  = one_hot_AA_seq(json["sequence"])
+        ohsty = one_hot_struct_seq(json["predicted_loop_type"])
+        ohst = one_hot_structure(json["structure"])
         data = numpy.concatenate((ohaa ,ohsty , ohst), axis= 1)
         data_out.append(data)
+        list_mat.append(mat)
     mat_out = numpy.array(data_out)
-    return(mat_out)
+    list_mat = numpy.array(list_mat)
+    print()
+    return(mat_out,list_mat)
 
 
 def mat_output(jsons):
     data_out = []
-    for i in jsons :
-        reactivity = i["reactivity"]
-        deg_Mg_pH10 = i["deg_Mg_pH10"]
-        deg_pH10 = i["deg_pH10"]
-        deg_Mg_50C = i["deg_Mg_50C"]
-        deg_50C = i["deg_50C"]
+    for i,json in enumerate(jsons) :
+        print("loading output : {:.2f} %".format((i+1)/len(jsons)*100),end = "\r")
+        reactivity = json["reactivity"]
+        deg_Mg_pH10 = json["deg_Mg_pH10"]
+        deg_pH10 = json["deg_pH10"]
+        deg_Mg_50C = json["deg_Mg_50C"]
+        deg_50C = json["deg_50C"]
         out = []
         for j in range(len(reactivity)):
              out.append([reactivity[j],
@@ -136,5 +143,28 @@ def mat_output(jsons):
                          deg_Mg_50C[j],
                          deg_50C[j]])
         data_out.append(out)
+    print()
     data_out = numpy.array(data_out)
     return data_out
+
+if __name__ == "__main__" :
+    jsons_train = []
+
+    with open("../data/train.json") as fil :
+        for i in fil :
+            a = json.loads(i)
+            if a["SN_filter"] == 0:
+                continue
+            jsons_train.append(a)
+ 
+            jsons_test = []
+
+    with open("../data/test.json") as fil :
+        for i in fil :
+            jsons_test.append(json.loads(i))
+
+
+    mat_intput_train,mat_input2 = mat_input(jsons_train)
+
+    Y_train = mat_output(jsons_train)
+    pass
--- src/resnet.py
+++ src/resnet.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-def classic(model_in):
-    model = layers.Conv1D(filters= 30,
+def classic(model_in,filters = 30):
+    model = layers.Conv1D(filters= filters,
                           kernel_size=(3,),
                           activation= "relu",
                           padding="same")(model_in)
-    model = layers.Conv1D(filters= 30,
+    model = layers.Conv1D(filters= filters,
                           kernel_size=(3,),
                           activation= "relu",
                           padding="same")(model)
-    model = layers.Conv1D(filters= 30,
+    model = layers.Conv1D(filters= filters,
                           kernel_size=(3,),
                           activation= "relu",
                           padding="same")(model)
@@ -19,14 +19,14 @@ def classic(model_in):
 
 
 
-def original(model_in):
-    model  = layers.Conv1D(filters = 30,
+def original(model_in,filters = 30):
+    model  = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model_in)
     model = layers.BatchNormalization()(model)
     model = layers.Activation("relu")(model)
-    model = layers.Conv1D(filters = 30,
+    model = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model)
@@ -35,16 +35,16 @@ def original(model_in):
     return model
 
 
-def pre_act(model_in):
+def pre_act(model_in,filters = 30):
     model = layers.BatchNormalization()(model_in)
     model = layers.Activation(activation="relu")(model)
-    model = layers.Conv1D(filters = 30,
+    model = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model)
     model = layers.BatchNormalization()(model)
     model = layers.Activation(activation="relu")(model)
-    model = layers.Conv1D(filters = 30,
+    model = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model)
@@ -52,15 +52,15 @@ def pre_act(model_in):
     return model
 
 
-def pre_act_mod(model_in):
+def pre_act_mod(model_in,filters = 30):
     model = layers.BatchNormalization()(model_in)
-    model = layers.Conv1D(filters = 30,
+    model = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model)
     model = layers.BatchNormalization()(model)
     model = layers.Activation(activation="relu")(model)
-    model = layers.Conv1D(filters = 30,
+    model = layers.Conv1D(filters = filters,
                            kernel_size=(3,),
                            activation = "relu",
                            padding="same")(model)
